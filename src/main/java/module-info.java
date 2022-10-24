1561cbee6500fadd8bb37e74d41c676e7b26173a
Silhin <serezha.yalovenko@gmail.com>
2022-10-24 15:11:16 +0300

    Added build support files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,14 +11,6 @@ module ru.silhin.imageconverter {
     exports ru.silhin.imageconverter.filter;
     exports ru.silhin.imageconverter.controller;
     opens ru.silhin.imageconverter.controller to javafx.fxml;
-    exports ru.silhin.imageconverter.filter.lab8;
-    exports ru.silhin.imageconverter.filter.lab7;
-    exports ru.silhin.imageconverter.filter.lab1;
-    exports ru.silhin.imageconverter.filter.lab2;
-    exports ru.silhin.imageconverter.filter.lab3;
-    exports ru.silhin.imageconverter.filter.lab4;
-    exports ru.silhin.imageconverter.filter.lab5;
-    exports ru.silhin.imageconverter.filter.lab6;
     exports ru.silhin.imageconverter.controller.config;
     opens ru.silhin.imageconverter.controller.config to javafx.fxml;
 }
---
+++ src/main/java/ru/silhin/imageconverter/Launcher.java
@@ -0,0 +1,7 @@
+package ru.silhin.imageconverter;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
